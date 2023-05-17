486e7cefe66caf1a3dbd41f9c23c559218a37003
Pedro Aznarez <pedroaznarez12@gmail.com>
2023-05-17 09:13:53 -0300

    Paragraphs and css

---
+++ components/FloatingWhatsApp/FloatingWhatsApp.jsx
@@ -0,0 +1,7 @@
+import { FloatingWhatsApp } from "react-floating-whatsapp";
+
+function App() {
+  return <FloatingWhatsApp onClick={""} phoneNumber={"59895469349"} />;
+}
+
+export default FloatingWhatsApp;
